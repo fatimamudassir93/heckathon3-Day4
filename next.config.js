8895f8b2f3d79bbb968bb865e7c024f8b9d914be
fatimamudassir93 <shiza.siddiq@gmail.com>
2025-02-08 22:03:48 +0500

    update

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["cdn.sanity.io"], // Sanity ke images ke liye domain allow karna zaroori hai
+//   },
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
